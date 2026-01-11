c795d850521357e6d66d14202ed3be15709a66b9
YedidyaHKfir <Yedidya@dono.ai>
2026-01-11 17:54:40 +0200

    fix init file

--- egl/__init__.py
+++ egl/__init__.py
@@ -1,18 +1,3 @@
-def __getattr__(name):
-    if name == "EGL":
-        from .egl import EGL
+from egl.alg_optimizer import minimize
 
-        return EGL
-    if name == "TRCovarianceMatrixAdaptation":
-        from .cma import TRCovarianceMatrixAdaptation
-
-        return TRCovarianceMatrixAdaptation
-    if name == "IGL":
-        from .igl import IGL
-
-        return IGL
-    if name == "minimize":
-        from .alg_optimizer import minimize
-
-        return minimize
-    raise AttributeError(f"module {__name__} has no attribute {name}")
+__all__ = ["minimize"]
